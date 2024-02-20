fae82327db5c252c6191cd05cb0c7c2c8e7f9d62
Artem <Skor_v5xz@student.itstep.org>
2024-02-20 18:23:01 +0100

    First

---
+++ SpanishFootballChampionship.DAL/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SpanishFootballChampionship.DAL
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ SpanishFootballChampionship.DAL/Team.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SpanishFootballChampionship.DAL
+{
+    internal class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int NumberOfWins {  get; set; }
+
+        public int NumberOfLoses { get; set; }
+
+        public int NumberOfDraws { get; set; }
+    }
+}
