026f547280f339592c04210d8d8aad5bda7a9df6
Artem <Skor_v5xz@student.itstep.org>
2024-02-23 17:52:59 +0100

    Модуль 6. Частина 2

--- SpanishFootballChampionship/Program.cs
+++ SpanishFootballChampionship/Program.cs
@@ -15,9 +15,9 @@ namespace FootballLeague.TestApp
                 //var team1 = new Team { Name = "Real Madrid", City = "Madrid", Wins = 20, Losses = 5, Draws = 3 };
                 //var team2 = new Team { Name = "Barcelona", City = "Barcelona", Wins = 18, Losses = 7, Draws = 3 };
 
-                context.Teams.Add(team1);
-                context.Teams.Add(team2);
-                context.SaveChanges();
+                //context.Teams.Add(team1);
+                //context.Teams.Add(team2);
+                //context.SaveChanges();
 
                 var teams = context.Teams;
                 foreach (var team in teams)
